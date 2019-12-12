8ad8703d005f441a0f22d0510d91b4542e1114cb
Jashele T <jashele@Jasheles-Air.domain>
2019-12-11 22:58:49 -0700

    added migration: users table

--- notesworkflow.js
+++ notesworkflow.js
@@ -1,12 +1,37 @@
 // NOTES WORKFLOW /SETUP
 
-//  B U I L D * T H E * A P I
-// run: npm init -y ...to generate a package.json
-// install the express module: yarn add express 
-// create server.js
-// create index.js 
-// add scripts to package json - 
-         //  "server": "nodemon index.js",
-         // "start": "node index.js"
-         // test that server runs , terminal + postman
+// P A R T 1 : Setting up Essentials for server /dependencies
 
+// run: npm init -y ...to generate a package.json
+// install the express module: yarn add express
+// create server.js
+// create index.js
+// add scripts to package json -
+            //  "server": "nodemon index.js",
+            // "start": "node index.js"
+// test that server runs , terminal + postman
+
+
+
+// P A R T 2 : Setting up Database Schema
+
+// Dependencies : npm add knex sqlite3
+      // to install globally: npm install -g knex
+      // you can also run npx knex
+
+// run : knex init ...creates knex file
+      // modify knex file with appropriate file paths ...
+      // + migrations + seeds + pool - https://github.com/jasheloper/node-db-challenge/blob/jashele-tillman/knexfile.js (sample)
+
+// Migrations 
+   // knex migrate:make create-users-table 
+      // add tables 
+
+
+// P A R T 3
+
+// P A R T 4
+
+// P A R T 5
+
+// P A R T 6
